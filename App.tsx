f1e626b1e25e633ffb68fb6d28a33e88dae42942
Nattapong Promthong <banker.z412@gmail.com>
2024-10-07 19:04:38 +0700

    feat: sync back in mobile and web

--- App.tsx
+++ App.tsx
@@ -1,56 +1,40 @@
-// import React, {useRef} from 'react';
-// import {SafeAreaView, Button, Alert} from 'react-native';
-// import {WebView} from 'react-native-webview';
-
-// const App = () => {
-//   const webviewRef = useRef(null);
-
-//   // Function to handle messages from the WebView
-//   const handleMessage = event => {
-//     const message = event.nativeEvent.data;
-//     Alert.alert('Message from WebView', message);
-//     console.log('Message from WebView', message);
-//   };
-
-//   // Function to send message to the WebView
-//   const sendMessageToWeb = () => {
-//     webviewRef.current.postMessage('Hello from React Native!');
-//   };
-
-//   return (
-//     <SafeAreaView style={{flex: 1}}>
-//       <Button title="Send Message to WebView" onPress={sendMessageToWeb} />
-//       <WebView
-//         ref={webviewRef}
-//         source={{uri: 'http://10.0.2.2:5173'}} // Change to your web server URL
-//         onMessage={handleMessage}
-//       />
-//     </SafeAreaView>
-//   );
-// };
-
-// export default App;
-
-// App.tsx
-import React, {useRef} from 'react';
-import {SafeAreaView, Button, Alert} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
+import {SafeAreaView, Button, Alert, BackHandler} from 'react-native';
 import {WebView, WebViewMessageEvent} from 'react-native-webview';
 
 const App: React.FC = () => {
-  const webviewRef = useRef<WebView>(null);
+  const webViewRef = useRef<WebView>(null);
 
-  // Handle messages from the WebView
+  const [canGoBack, setCanGoBack] = useState(false);
+
+  useEffect(() => {
+    const backAction = () => {
+      if (canGoBack) {
+        // @ts-ignore
+        webViewRef.current.goBack();
+        return true;
+      }
+      return false;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      backAction,
+    );
+
+    return () => backHandler.remove();
+  }, [canGoBack]);
+
   const handleMessage = (event: WebViewMessageEvent) => {
     const message = event.nativeEvent.data;
     Alert.alert('Message from WebView', message);
     console.log('Message from WebView', message);
   };
 
-  // Send a message to the WebView
   const sendMessageToWeb = () => {
-    if (webviewRef.current) {
+    if (webViewRef.current) {
       console.log('Sending message to WebView');
-      webviewRef.current.postMessage('Hello from React Native!');
+      webViewRef.current.postMessage('Hello from React Native!');
     } else {
       console.error('WebView reference is null.');
     }
@@ -60,9 +44,10 @@ const App: React.FC = () => {
     <SafeAreaView style={{flex: 1}}>
       <Button title="Send Message to WebView" onPress={sendMessageToWeb} />
       <WebView
-        ref={webviewRef}
+        ref={webViewRef}
         source={{uri: 'http://10.0.2.2:5173'}} // Change to your web server URL
         onMessage={handleMessage}
+        onNavigationStateChange={navState => setCanGoBack(navState.canGoBack)}
       />
     </SafeAreaView>
   );
